b9072b03395e9cd715d41b80053dab1d5b97b190
Roshan Mohapatra <roshanmohapatra2004@gmail.com>
2025-11-07 03:06:24 +0530

    modified shell.cpp file and made it executable to commands

--- shell.cpp
+++ shell.cpp
@@ -2,6 +2,10 @@
 #include<string>
 #include<sstream>
 #include<vector>
+#include<unistd.h>
+#include<sys/types.h>
+#include <process.h>
+#include <errno.h>
 
 using namespace std;
 
@@ -22,9 +26,10 @@ int main(){
     while (true)
     {
         cout<<"cshell>";    //shell prompt
-        getline(cin,input); //get inputline
+        if(!getline(cin,input)) break; //get inputline
 
-        if(cin.eof() || input=="exit"){ //code to check for quitting shell
+        if(cin.eof() || input=="exit" || input=="EXIT" || input=="Exit"){ //code to check for quitting shell
+            cout<<"Exiting shell..."<<endl;
             break;
         }
         vector<string> tokens =parseInput(input);   //parsing input to tokens
@@ -32,13 +37,39 @@ int main(){
         if(tokens.empty())
             continue;
         
+
+        //convert tokens to c-style array for executimug
+        vector<char*> cmdArgs;
+        for(auto &arg:tokens){
+            cmdArgs.push_back(&arg[0]);
+        }
+        cmdArgs.push_back(NULL);
+
+
+// Special case for internal Windows commands like dir, cd, cls
+if(tokens[0] == "dir" || tokens[0] == "cls"){
+    vector<char*> cmdArgs2 = { (char*)"cmd", (char*)"/c", &tokens[0][0], NULL };
+    _spawnvp(_P_WAIT, "cmd", cmdArgs2.data());
+    continue;
+}
+
+// Normal external commands
+int result = _spawnvp(_P_WAIT, cmdArgs[0], cmdArgs.data());
+if(result == -1) {
+    perror("Command execution failed!");
+}
+        if(result==-1){
+            perror("Command execution failed!");
+        }
+
+        /*
+        For Showing Tokens
         cout<<"Parsed tokens: ";    
         for (const string &t : tokens){     //showing parsed tokens 
             cout<<"["<<t<<"]";
         }
-        cout<<endl;
+        cout<<endl;*/
     }
-    cout<<"Existing shell..."<<endl;
     return 0;
     
 }
